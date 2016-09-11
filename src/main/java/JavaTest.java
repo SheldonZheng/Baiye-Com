4d92629ca9413958ead4f939633c63caee8f7d3b
SheldonZheng <zhenghangtxdyr@gmail.com>
2016-09-11 22:45:59 +0800

    Initial commit

---
+++ src/main/java/JavaTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Baiye on 9/11/16.
+ */
+public class JavaTest {
+    public static void main(String args[]) {
+        System.out.println("xxx");
+    }
+}
---
+++ src/main/scala/Test.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by Baiye on 9/11/16.
+  */
+object Test {
+  def main(args: Array[String]): Unit = {
+    print("xxx")
+  }
+}
