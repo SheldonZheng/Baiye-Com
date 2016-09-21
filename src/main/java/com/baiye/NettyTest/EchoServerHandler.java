7ac98007747d453c58237dc6d633b8e4085592e8
Baiye <zhenghangtxdyr@gmail.com>
2016-09-21 17:57:55 +0800

    修改目录结构；

--- src/main/java/com/baiye/NettyTest/EchoServerHandler.java
+++ src/main/java/com/baiye/NettyTest/EchoServerHandler.java
@@ -1,3 +1,5 @@
+package com.baiye.NettyTest;
+
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import org.slf4j.Logger;
---
+++ src/main/scala/EchoServer.scala
@@ -0,0 +1,16 @@
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.EventLoopGroup
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.nio.NioServerSocketChannel
+
+/**
+  * Created by Baiye on 2016/9/19.
+  */
+class EchoServer {
+
+  def run(): Unit =
+  {
+
+  }
+
+}
