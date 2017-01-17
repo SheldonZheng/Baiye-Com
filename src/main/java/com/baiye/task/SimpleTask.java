fe29c6c61a127505fe2bbae989636c8790c2b86f
Baiye <zhenghangtxdyr@gmail.com>
2017-01-17 17:30:29 +0800

    加入SimpleTask;

---
+++ src/main/java/com/baiye/task/SimpleTask.java
@@ -0,0 +1,62 @@
+package com.baiye.task;
+
+import com.baiye.annotation.BaiyeTaskMethod;
+import com.baiye.helper.ClassHelper;
+import com.baiye.util.ClassUtil;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Set;
+
+/**
+ * Created by Baiye on 2017/1/17.
+ */
+public class SimpleTask implements Runnable{
+
+    private static final Logger logger = LoggerFactory.getLogger(SimpleTask.class);
+
+
+    private String packageName;
+
+    public SimpleTask(String packageName)
+    {
+        this.packageName = packageName;
+    }
+
+    @Override
+    public void run() {
+        Set<Class<?>> classSet = ClassHelper.getBaiyeTaskClassAnnotation(packageName);
+        if(CollectionUtils.isEmpty(classSet))
+            return;
+
+        for (Class<?> cls : classSet) {
+            Object classInstance = null;
+            try {
+                classInstance = cls.newInstance();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            Method[] methods = cls.getMethods();
+            for (Method method : methods) {
+                if(method.isAnnotationPresent(BaiyeTaskMethod.class))
+                {
+                    try {
+                        method.invoke(classInstance,new Object[]{});
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
+
+    }
+}
