32e48be34b3a4456ae4d4f59038914c24d4c7899
Baiye <zhenghangtxdyr@gmail.com>
2017-02-03 11:43:00 +0800

    优化依赖,避免重复依赖;

--- src/main/scala/com/baiye/helper/LoggerHelper.scala
+++
@@ -1,9 +0,0 @@
-package com.baiye.helper
-
-/**
-  * Created by Baiye on 2017/1/17.
-  */
-object LoggerHelper {
-
-
-}
--- src/test/scala/ScalaTestClass.scala
+++
@@ -1,6 +0,0 @@
-/**
-  * Created by Baiye on 2016/9/12.
-  */
-class ScalaTestClass {
-
-}
