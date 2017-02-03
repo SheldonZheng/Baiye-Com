f841cc39b314a902f3ad21cb3bfd6f9273636196
Baiye <zhenghangtxdyr@gmail.com>
2017-02-03 15:18:29 +0800

    加入runningTasks接口,用于获取正在运行的任务;

--- src/main/java/com/baiye/Main.java
+++ src/main/java/com/baiye/Main.java
@@ -2,8 +2,13 @@ package com.baiye;
 
 
 import com.baiye.common.helper.HttpHelper;
+import com.baiye.container.Container;
+import com.baiye.container.SchedulerTaskLocalContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 /**
@@ -15,12 +20,6 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
-        /*ExecutorService executorService  = Executors.newFixedThreadPool(4);
-        Container container = new SchedulerTaskLocalContainer("com.baiye.test.Task");
-        executorService.execute(container);
-        container = new SimpleTaskLocalContainer("com.baiye.test.Task");
-        executorService.execute(container);
-        executorService.shutdown();*/
     }
 
 }
--- src/main/java/com/baiye/controller/SystemController.java
+++ src/main/java/com/baiye/controller/SystemController.java
@@ -1,8 +1,16 @@
 package com.baiye.controller;
 
+import com.baiye.single.SingleMapEnum;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ScheduledFuture;
 
 /**
  * Created by Baiye on 01/02/2017.
@@ -15,5 +23,20 @@ public class SystemController {
     @ResponseBody
     public String healthCheck(){
         return "ok";
+    }
+
+    @RequestMapping("/runningTasks")
+    @ResponseBody
+    public List<String> runningTasks()
+    {
+        List<String> result = Lists.newArrayList();
+        Map<String,ScheduledFuture> scheduledFutureMap = SingleMapEnum.LocalTaskFutureSingleMap.getMap();
+        if(scheduledFutureMap != null && scheduledFutureMap.size() > 0)
+        {
+            for (Map.Entry<String, ScheduledFuture> entry : scheduledFutureMap.entrySet()) {
+                result.add(entry.getKey());
+            }
+        }
+        return result;
     }
 }
