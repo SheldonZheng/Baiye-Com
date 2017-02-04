0fa5e954df40ecf7d7b4c7369eccc70ed30cc477
Baiye <zhenghangtxdyr@gmail.com>
2017-02-04 15:58:47 +0800

    自定义上传Jar包加入任务的方式单元测试通过;

--- Baiye-Task-Core/src/test/java/com/baiye/test/taskTest/SchedulerTaskLocalContainerTest.java
+++ Baiye-Task-Core/src/test/java/com/baiye/test/taskTest/SchedulerTaskLocalContainerTest.java
@@ -21,14 +21,38 @@ public class SchedulerTaskLocalContainerTest {
     @SchedulerTask(name = "test1",firstDelay = 1000,delay = 1000)
     public void test()
     {
-        System.out.println("1SchedulerTaskLocalContainer test success!");
+        System.out.println("SchedulerTaskLocalContainer test success!");
     }
 
-    @Test
+   @Test
     public void schedulerTaskTest()
     {
         Container container = new SchedulerTaskLocalContainer();
         container.addTasks("com.baiye.test.taskTest");
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Map<String,ScheduledFuture> scheduledFutureMap = SingleMapEnum.LocalTaskFutureSingleMap.getMap();
+        for (Map.Entry<String, ScheduledFuture> entry : scheduledFutureMap.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            try {
+                Thread.sleep(5000);
+                entry.getValue().cancel(false);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void addTaskTest()
+    {
+        Container container = new SchedulerTaskLocalContainer();
+        container.addTasks("com.baiye.test.taskTest1","D:\\test.jar");
 
         try {
             Thread.sleep(1000);
