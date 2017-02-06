c4b31b7a44d120f77eea1b4e5329e4158d0ad217
Baiye <zhenghangtxdyr@gmail.com>
2017-02-06 10:44:35 +0800

    支持访问/直接跳转到上传JAR包页面;

--- Baiye-Task-Core/src/main/java/com/baiye/Main.java
+++ Baiye-Task-Core/src/main/java/com/baiye/Main.java
@@ -3,22 +3,16 @@ package com.baiye;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by Baiye on 2017/1/17.
  */
 
 @SpringBootApplication
-public class Main extends SpringBootServletInitializer {
+public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Main.class);
-    }
 }
