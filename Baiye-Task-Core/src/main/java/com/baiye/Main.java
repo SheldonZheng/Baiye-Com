a0079424838286ccb61dadb1f8afbee98d19a66d
Baiye <zhenghangtxdyr@gmail.com>
2017-02-07 13:43:29 +0800

    加入Owner依赖;

--- Baiye-Task-Core/src/main/java/com/baiye/Main.java
+++ Baiye-Task-Core/src/main/java/com/baiye/Main.java
@@ -4,6 +4,7 @@ package com.baiye;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 /**
  * Created by Baiye on 2017/1/17.
  */
