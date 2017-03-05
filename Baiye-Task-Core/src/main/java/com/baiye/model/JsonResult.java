68436e5e7a3ef70c63d1c7238f23667771a03bdf
Baiye <zhenghangtxdyr@gmail.com>
2017-03-05 10:20:02 +0800

    为JsonResult加入buildSuccessResult方法;

--- Baiye-Task-Core/src/main/java/com/baiye/model/JsonResult.java
+++ Baiye-Task-Core/src/main/java/com/baiye/model/JsonResult.java
@@ -1,19 +1,28 @@
 package com.baiye.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+
+import java.io.Serializable;
 
 /**
  * Created by Baiye on 2017/3/5.
  */
 @Data
 @Builder
-public class JsonResult<T> {
+@AllArgsConstructor
+public class JsonResult<T> implements Serializable{
 
     private String msg;
 
     private String code;
 
     private T t;
+
+    public static JsonResult buildSuccessResult(Object data)
+    {
+        return new JsonResult("success","0",data);
+    }
 
 }
