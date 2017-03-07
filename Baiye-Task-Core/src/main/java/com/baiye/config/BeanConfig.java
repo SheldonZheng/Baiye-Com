ee99d548003b322718dd20194bbf03e6d8784af4
hang.zheng <hang.zheng@quantgroup.cn>
2017-03-07 18:47:03 +0800

    实现getClassLoader方法,优先从缓存中获取缓存的ClassLoader;

--- Baiye-Task-Core/src/main/java/com/baiye/config/BeanConfig.java
+++ Baiye-Task-Core/src/main/java/com/baiye/config/BeanConfig.java
@@ -1,22 +1,41 @@
 package com.baiye.config;
 
-import com.google.common.collect.Maps;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Baiye on 2017/3/5.
  */
-@Component
+@Configuration
 public class BeanConfig {
 
     @Bean("taskFutureMap")
     public ConcurrentHashMap<String, ScheduledFuture> getTaskFutureMap()
     {
         return new ConcurrentHashMap<String,ScheduledFuture>();
+    }
+
+    @Bean("classLoaderCache")
+    public LoadingCache<String,ClassLoader> classLoaderCache()
+    {
+        return CacheBuilder
+                .newBuilder()
+                .expireAfterAccess(1, TimeUnit.DAYS)
+                .build(new CacheLoader<String, ClassLoader>() {
+                    @Override
+                    public ClassLoader load(String key) throws Exception {
+                        return null;
+                    }
+                });
+
     }
 
 }
